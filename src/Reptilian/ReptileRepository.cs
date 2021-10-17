b453224761f68f34bd7880f446fb586004454d64
Lache Melvin <lache.melvin@gmail.com>
2021-10-17 01:31:09 +0000

    add use of database transactions

--- src/Reptilian/ReptileRepository.cs
+++ src/Reptilian/ReptileRepository.cs
@@ -12,21 +12,41 @@ namespace Reptilian
 
         public Guid Save(Reptile reptile)
         {
-            using (var session = _sessionFactory.OpenSession())
+            return InTx(session =>
             {
-                var id = (Guid)session.Save(reptile);
-                session.Flush();
-                session.Close();
-                return id;
-            }
+                return (Guid)session.Save(reptile);
+            });
         }
 
         public Reptile GetById(Guid id)
         {
+            return InTx(session =>
+            {
+                return session.Get<Reptile>(id);
+            });
+        }
+
+        protected T InTx<T>(Func<ISession, T> function)
+        {
             using (var session = _sessionFactory.OpenSession())
             {
-                var reptile = session.Get<Reptile>(id);
-                return reptile;
+                using (var tx = session.BeginTransaction())
+                {
+                    T result;
+                    try
+                    {
+                        result = function.Invoke(session);
+                        tx.Commit();
+                        return result;
+                    }
+#pragma warning disable CS0168 // variable declared but never used
+                    catch (Exception ex)
+                    {
+                        // TODO: include retry logic in exception handling
+                        tx.Rollback();
+                        throw;
+                    }
+                }
             }
         }
 
